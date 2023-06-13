030a17cd5291ab5e724e14b7f767b5fc818bb879
TJ Stifter <tjstifter@gmail.com>
2023-06-13 14:20:04 -0400

    completes lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+
+function focus(){
+    console.log('Good!')
+}
+
+function blur(){
+    console.log("Hey! Eyes on me!")
+}
+function EyesOnMe(){
+    return (<div>
+        <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    </div>)
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function seeWhatHappens(event){
+        console.log(event.target.value);
+    }
+
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={enterPass}></input>
+        </div>
     )
+}
+
+function enterPass()
+{
+    console.log("Entering password...")
 }
 
 export default Keypad;
